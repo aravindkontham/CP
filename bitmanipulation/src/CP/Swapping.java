24fca954cda2263689f9d912d54907f11e42e009
aravindkontham <aravindkontham11@gmail.com>
2024-01-13 16:52:39 +0530

    Swapping two numbers using bit manipulation

--- bitmanipulation/src/CP/Swapping.java
+++ bitmanipulation/src/CP/Swapping.java
@@ -1,4 +1,13 @@
 package CP;
 
 public class Swapping {
+    public static void main(String[] args) {
+        int a =10;
+        int b=5;
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println(a);
+        System.out.print(b);
+    }
 }
